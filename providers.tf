2dee709470008d2c90d5c97106358f52ef573f59
Bernd Malmqvist <bernd@lehmann.be>
2019-03-24 16:24:09 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "techbloc-terraform-data"
+    key    = "openshift-311"
+    region = "eu-west-1"
+  }
+}
+
 data "aws_region" "current" {}
 
 data "aws_availability_zones" "available" {}
