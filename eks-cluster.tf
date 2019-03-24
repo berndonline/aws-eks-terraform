5c9a40c030fa7cad6fbf341d86bca5ae687033be
Bernd Malmqvist <bernd@lehmann.be>
2019-03-24 16:25:52 +0000

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -56,7 +56,7 @@ resource "aws_eks_cluster" "eks" {
   role_arn = "${aws_iam_role.eks-cluster.arn}"
   vpc_config {
     security_group_ids = ["${aws_security_group.eks-cluster.id}"]
-    subnet_ids         = ["${aws_subnet.demo.*.id}"]
+    subnet_ids         = ["${aws_subnet.eks.*.id}"]
   }
   depends_on = [
     "aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy",
