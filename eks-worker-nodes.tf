682fc50b4059416922c7ce4f87856933438ea26b
Bernd Malmqvist <bernd@lehmann.be>
2019-05-11 17:26:26 +0100

    Update eks-worker-nodes.tf

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -115,9 +115,9 @@ resource "aws_launch_configuration" "eks" {
 }
 
 resource "aws_autoscaling_group" "eks" {
-  desired_capacity     = 8
+  desired_capacity     = 10
   launch_configuration = "${aws_launch_configuration.eks.id}"
-  max_size             = 8
+  max_size             = 10
   min_size             = 1
   name                 = "eks"
   vpc_zone_identifier  = ["${aws_subnet.eks.*.id}"]
